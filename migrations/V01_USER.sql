c558f7d825f0e9bdadd7e5542e6c4d38ae65aef1
Rasika1055 <deshmukhrasika56@gmail.com>
2023-07-21 15:59:01 +0530

    check

--- migrations/V01_USER.sql
+++
@@ -1,6 +0,0 @@
-create view SFDB.DEMO.V01_USER
-<<<<<<< HEAD
-as Select * from SFDB.DEMO.USER;
-=======
-as Select * from SFDB.DEMO.USER;
->>>>>>> origin
--- migrations/V1.1.1__initial_objects.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA DEMO;
-CREATE TABLE USER
-(
-   FIRST_NAME VARCHAR
-  ,LAST_NAME VARCHAR
-);
-
-INSERT INTO SFDB.DEMO.USER values('Rasika','Deshmukh'),('Aryan','Deshmukh');
